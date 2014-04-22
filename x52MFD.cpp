6fb92c5a6c48eddf8e925f83ef10565baa9f3f5b
wentam <wentam42@gmail.com>
2014-04-22 17:53:13 -0400

    add mode support (no interface to change mode yet)

--- x52MFD.cpp
+++ x52MFD.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 
+enum Mode {COM,NAV,ADF,DME};
 
 using namespace std;
 	int
@@ -15,10 +16,31 @@ main ( int argc, char *argv[] )
 	UdpServer server;
 	MfdPage page(mymsg);
 	MFD mfd;
+    Mode mode = COM;
 
 	for(;;){ //continuously update the mfd with data from flightgear
-		std::vector<Msg> vec = parser.parse(server.fetch()); 
-		Msg mymsg2(vec[0]);
+		std::vector<Msg> vec = parser.parse(server.fetch());
+
+		Msg mymsg2;
+       
+        switch (mode)
+        {
+            case COM:
+                mymsg2 = vec[0];
+                break;
+            case ADF:
+                mymsg2 = vec[1];
+                break;
+            case NAV:
+                mymsg2 = vec[2];
+                break;
+            case DME:
+                mymsg2 = vec[3];
+                break;
+            default:
+                break;
+        }
+
 		MfdPage page2(mymsg2);
 		mfd.setPage(page2);
 
